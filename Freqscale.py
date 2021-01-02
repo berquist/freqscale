f649d380222815b1308d576c12fba958f74e8670
Eric Berquist <eric.berquist@gmail.com>
2021-01-02 00:21:04 -0500

    first rewrite of Freqscale.py

--- Freqscale.py
+++ Freqscale.py
@@ -1,44 +1,82 @@
-# This program is used to calculate the Scale Factors for Zero-Point Energies, Harmonic Frequencies, and Fundamental Frequencies. 
+import json
+from pathlib import Path
+from typing import Iterable, Tuple
 
-import random
-import sys
-import os
+import numpy as np
 
-NUMER = 0.0   # numerator in eq. 5 of the main paper
-DENOM = 0.0   # denominator in eq. 5 of the main paper
-LAMBDA = 0.0  # lamda_ZPE on the left side of eq. 5 of the main paper
+from cclib.io import ccread
+from cclib.parser.utils import convertor
 
-#read in calculated and experimental frequencies
-with open('freqcom.txt') as f:
-    f=[x.strip() for x in f if x.strip()]
-    data=[tuple(map(float,x.split())) for x in f[0:]]
-    zpecom=[x[0] for x in data]
-if len(zpecom) == 15:
-#calculate scale factor for Zero-Point Energies for the full model
-        zpeexp = (16.490, 27.710, 7.3, 3.0929144, 1.302, 16.1, 13.26, 6.231, 10.000, 5.864, 6.770, 3.3618, 21.200, 5.2915, 0.7983)
-        for i in range(0,15):
-                NUMER = NUMER + zpecom[i]*zpeexp[i]
-                DENOM = DENOM + zpecom[i]**2.0
-else:
-#calculate scale factor for Zero-Point Energies for the reduced model        
-        zpeexp = (16.490, 27.710, 16.1, 13.26, 6.770, 21.200)
-        for i in range(0,6):
-                NUMER = NUMER + zpecom[i]*zpeexp[i]
-                DENOM = DENOM + zpecom[i]**2.0
-LAMBDA = NUMER/DENOM
+_SCALE_ZPE = 1.0
+_SCALE_HARM = 1.014
+_SCALE_FUND = 0.974
 
-#print the results to the screen
-
-print('FREQ: A PROGRAM FOR OPTIMIZING SCALE FACTORS (Version 1)\n')
-print('                 written by                 \n')
-print('Haoyu S. Yu, Lucas J. Fiedler, I.M. Alecu, and Donald G. Truhlar\n')
-print('Department of Chemistry and Supercomputing Institute\n')
-print('University of Minnesota, Minnesota 55455-0431\n')
-print('Scale Factor for Zero-Point Energies     =',LAMBDA,'\n')
-print('Scale Factor for Harmonic Frequencies    =',LAMBDA*1.014,'\n')
-print('Scale Factor for Fundamental Frequencies =',LAMBDA*0.974,'\n')
-print('CITATIONS:\n')
-print('1. Alecu, I.M., Zheng, J., Zhao, Y., and Truhlar, D.G. J. Chem. Theory Comput. 2010, 6, 2872\n')
-print('2. Yu, S.H., Fiedler, L.J., Alecu, I.M., and Truhlar, D.G. Comput. Phys.Commun. submittted 2016\n')
+COMPUTED_DATA_FILENAME = "computed_data.json"
 
 
+def extract_and_dump_computed_data(systems: Iterable[str]) -> None:
+    with open(COMPUTED_DATA_FILENAME, "w") as fh:
+        json.dump(
+            {system: extract_computed_zpe(f"{system}.out") for system in systems}, fh
+        )
+
+
+def extract_computed_zpe(filename: str) -> float:
+    """Get the computed ZPVE from the given quantum chemical output filename in
+    units of kcal/mol.
+    """
+    return convertor(ccread(filename).zpve, "hartree", "kcal/mol")
+
+
+def calculate_lambda(
+    experimental_zpves: Iterable[float], computed_zpves: Iterable[float]
+) -> float:
+    return np.dot(experimental_zpves, computed_zpves) / np.sum(
+        np.asanyarray(computed_zpves) ** 2.0
+    )
+
+
+def form_scaling_factors(lmbda: float) -> Tuple[float, float, float]:
+    return lmbda * _SCALE_ZPE, lmbda * _SCALE_HARM, lmbda * _SCALE_FUND
+
+
+def format_output(scaling_factors: Tuple[float, float, float]) -> str:
+    return "\n".join(
+        [
+            "FREQ: A PROGRAM FOR OPTIMIZING SCALE FACTORS (Version 1",
+            "                 written by                 ",
+            "Haoyu S. Yu, Lucas J. Fiedler, I.M. Alecu, and Donald G. Truhlar",
+            "Department of Chemistry and Supercomputing Institute",
+            "University of Minnesota, Minnesota 55455-0431",
+            f"Scale Factor for Zero-Point Energies     = {scaling_factors[0]}",
+            f"Scale Factor for Harmonic Frequencies    = {scaling_factors[1]}",
+            f"Scale Factor for Fundamental Frequencies = {scaling_factors[2]}",
+            "CITATIONS:",
+            "1. Alecu, I.M., Zheng, J., Zhao, Y., and Truhlar, D.G. J. Chem. Theory Comput. 2010, 6, 2872",
+            "2. Yu, S.H., Fiedler, L.J., Alecu, I.M., and Truhlar, D.G. Comput. Phys.Commun. submittted 2016",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=("dump", "compute"))
+    parser.add_argument("model_type", choices=("full", "reduced"))
+    args = parser.parse_args()
+
+    systems = json.loads(Path(f"molecules_{args.model_type}.json").read_text())
+
+    if args.action == "dump":
+        extract_and_dump_computed_data(systems)
+    elif args.action == "compute":
+        all_experimental_zpves = json.loads(Path("experimental_data.json").read_text())
+        used_experimental_zpves = [all_experimental_zpves[system] for system in systems]
+        all_computed_zpves = json.loads(Path(COMPUTED_DATA_FILENAME).read_text())
+        used_computed_zpves = [all_computed_zpves[system] for system in systems]
+
+        lmbda = calculate_lambda(used_experimental_zpves, used_computed_zpves)
+        scaling_factors = form_scaling_factors(lmbda)
+        # scale_zpe, scale_harm, scale_fund = scaling_factors
+        print(format_output(scaling_factors))
---
+++ README.md
@@ -0,0 +1,13 @@
+# FREQ
+
+This is a reimplementation of the FREQ code distributed through https://comp.chem.umn.edu/freq/.
+
+The program citation is
+
+> "Computational Thermochemistry: Automated Generation of Scale Factors for Vibrational Frequencies Calculated by Electronic Structure Model Chemistries," H. S. Yu, L. J. Fiedler, I. M. Alecu, and D. G. Truhlar, Computer Physics Communications 210, 132-138 (2017). https://doi.org/10.1016/j.cpc.2016.09.004
+
+and the scale factor optimization model originates from
+
+> "Computational Thermochemistry: Scale Factor Databases and Scale Factors for Vibrational Frequencies Obtained from Electronic Model Chemistries," I. M. Alecu, J. Zheng, Y. Zhao, and D. G. Truhlar, Journal of Chemical Theory and Computation 6, 2872-2887 (2010). https://doi.org/10.1021/ct100326h
+
+The original source code as downloaded on 2020-12-27 can be found as the first commit in this repository.
