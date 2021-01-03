1b373058a9b9b3c874e3d63d8625e709d17f158d
Eric Berquist <eric.berquist@gmail.com>
2021-01-03 13:03:03 -0500

    Separate out charge and multiplicity; allow Q-Chem usage

--- input.py
+++ input.py
@@ -1,20 +1,68 @@
-# This program is used to generate gaussian input files for the full model (with 15 molecules)
+from templates import header_gaussian, molecule_blocks
 
-# read in functional, basis set, and path of the basis set
-functional = str(input("Which method are you going to choose? "))
-basis = str(input("Which basis set are you going to choose? "))
-path = str(input("What is the path of your basis set? "))
 
-from templates import header, molecule_blocks
-
-def generate_output(name: str, basis: str, path: str) -> None:
+def write_output_gaussian(name: str, method: str, basis: str, path: str) -> None:
     with open(f"{name}.in", "w") as fh:
+        molecule = molecule_blocks[name]
         fh.write(header)
-        fh.write(f"{functional}/{basis}\n")
+        fh.write(f"{method}/{basis}\n")
         fh.write("\nt\n\n")
-        fh.write(molecule_blocks[name])
+        fh.write(f"{molecule['charge']},{molecule['multiplicity']}\n")
+        fh.write(molecule["block"])
         fh.write(f"\n{path}\n")
         fh.write("\n")
 
-for name in molecule_blocks:
-    generate_output(name, basis, path)
+def write_output_qchem(name: str, method: str, basis: str, path: str) -> None:
+    molecule = molecule_blocks[name]
+    inp = f"""$rem
+jobtype = opt
+method = {method}
+basis = {basis}
+mem_static = 1000
+mem_total = 4000
+geom_opt_tol_energy = 1
+geom_opt_tol_gradient = 40
+$end
+
+$molecule
+{molecule['charge']} {molecule['multiplicity']}
+
+{molecule['block']}
+$end
+
+@@@@
+
+$rem
+jobtype = freq
+method = {method}
+basis = {basis}
+mem_static = 1000
+mem_total = 4000
+$end
+
+$molecule
+read
+$end
+"""
+    with open(f"{name}.in", "w") as fh:
+        fh.write(inp)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("program", choices=("gaussian", "qchem"))
+    parser.add_argument("method")
+    parser.add_argument("basisset")
+    parser.add_argument("--basisset-path", default="")
+    args = parser.parse_args()
+
+    # TODO use pnictogen
+    if args.program == "gaussian":
+        writer = write_output_gaussian
+    elif args.program == "qchem":
+        writer = write_output_qchem
+
+    for name in molecule_blocks:
+        writer(name, args.method, args.basisset, args.basisset_path)
--- templates.py
+++ templates.py
@@ -1,10 +1,12 @@
-header = """%mem=420mb
+header_gaussian = """%mem=420mb
 # opt=(vtight,Maxcycle=200) scf=(xqc,maxcycle=400,tight) Freq integral=ultrafine
 """
 
 molecule_blocks = {
-    "c2h2": """0,1
-C
+    "c2h2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """C
 C,1,CC
 X,2,1.,1,90.
 H,2,CH,3,90.,1,180.,0
@@ -14,9 +16,11 @@ H,1,CH,5,90.,2,180.,0
 CC=1.203142
 CH=1.062605
 """,
-    "ch4": """0,1
-
-C
+    },
+    "ch4": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """C
 H,1,RCH
 H,1,RCH,2,AHCH
 H,1,RCH,2,AHCH,3,AHCH,1
@@ -25,98 +29,127 @@ H,1,RCH,2,AHCH,3,AHCH,-1
 RCH=1.08744517
 AHCH=109.471221
 """,
-    "cl2": """0,1
-Cl
+    },
+    "cl2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """Cl
 Cl,1,R
 
 R=1.1
 """,
-    "co": """0,1
-O
+    },
+    "co": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """O
 C,1,RCO
 
 RCO=1.12960815
 """,
-    "co2": """0,1
-
-C
+    },
+    "co2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """C
 O,1,R
 X,1,1.000000,2,90.000000
 O,1,R,3,90.000000,2,180.000000,0
 
 R=1.1594846
 """,
-    "f2": """0,1
-
-F
+    },
+    "f2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """F
 F,1,FF
 
 FF=1.3952041
 """,
-    "h2": """0,1
-
-H
+    },
+    "h2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """H
 H,1,r
 
 r=0.74187646
 """,
-    "h2co": """0,1
-
-O        0.000000    0.000000    0.674622
+    },
+    "h2co": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """O        0.000000    0.000000    0.674622
 C        0.000000    0.000000   -0.529707
 H        0.000000    0.935488   -1.109367
 H        0.000000   -0.935488   -1.109367
 """,
-    "h2o": """0,1
-
-O
+    },
+    "h2o": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """O
 H,1,OH
 H,1,OH,2,HOH
 
 OH=0.95691441
 HOH=104.51706026
 """,
-    "hcn": """0,1
-
-C        0.000000    0.000000   -0.500365
+    },
+    "hcn": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """C        0.000000    0.000000   -0.500365
 N        0.000000    0.000000    0.652640
 H        0.000000    0.000000   -1.566291
 """,
-    "hf": """0,1
-
-F
+    },
+    "hf": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """F
 H,1,R
 
 R=0.91538107
 """,
-    "n2": """0,1
-
-N
+    },
+    "n2": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """N
 N,1,NN
 
 NN=1.09710935
 """,
-    "n2o": """0,1
-
-N
+    },
+    "n2o": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """N
 N,1,r1
 O,2,r2,1,180.
 
 r1=1.12056262
 r2=1.1870483
 """,
-    "nh3": """0,1
-
-N        0.000000    0.000000    0.112890
+    },
+    "nh3": {
+        "charge": 0,
+        "multiplicity": 1,
+        "block": """N        0.000000    0.000000    0.112890
 H        0.000000    0.938024   -0.263409
 H        0.812353   -0.469012   -0.263409
 H       -0.812353   -0.469012   -0.263409
 """,
-    "oh": """0,2
-
-O
+    },
+    "oh": {
+        "charge": 0,
+        "multiplicity": 2,
+        "block": """O
 H,1,R
 
 R=0.967
 """,
+    },
 }
